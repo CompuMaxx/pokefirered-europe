388ba4e2dd1558abd96e87fb82c4a383cf4d20ba
Acimut <79547805+Acimut@users.noreply.github.com>
2024-11-01 22:35:29 -0500

    Update easy_chat_group_trainer.h

--- src/data/easy_chat/easy_chat_group_trainer.h
+++ src/data/easy_chat/easy_chat_group_trainer.h
@@ -1,4 +1,5 @@
 
+#if ENGLISH
 static const u8 sEasyChatWord_IChooseYou[] = _("I CHOOSE YOU");
 static const u8 sEasyChatWord_Gotcha[] = _("GOTCHA");
 static const u8 sEasyChatWord_Trade[] = _("TRADE");
@@ -155,6 +156,168 @@ static const struct EasyChatWordInfo sEasyChatGroup_Trainer[] = {
     {
         .text = sEasyChatWord_Silver,
         .alphabeticalOrder = 12,
+        .enabled = TRUE
+    }
+};
+
+#elif SPANISH
+static const u8 sEasyChatWord_IChooseYou[] = _("TE ELIJO");
+static const u8 sEasyChatWord_Gotcha[] = _("HAZTE");
+static const u8 sEasyChatWord_Trade[] = _("INTERCAMBIA");
+static const u8 sEasyChatWord_Sapphire[] = _("ZAFIRO");
+static const u8 sEasyChatWord_Evolve[] = _("EVOLUCIONAR");
+static const u8 sEasyChatWord_Encyclopedia[] = _("ENCICLOPEDIA");
+static const u8 sEasyChatWord_Nature[] = _("NATURALEZA");
+static const u8 sEasyChatWord_Center[] = _("CENTRO");
+static const u8 sEasyChatWord_Egg[] = _("HUEVO");
+static const u8 sEasyChatWord_Link[] = _("CONEXIÓN");
+static const u8 sEasyChatWord_SpAbility[] = _("HAB. ESP.");
+static const u8 sEasyChatWord_Trainer[] = _("ENTRENADOR");
+static const u8 sEasyChatWord_Version[] = _("VERSIÓN");
+static const u8 sEasyChatWord_Pokenav[] = _("POKéNAV");
+static const u8 sEasyChatWord_Pokemon[] = _("POKéMON");
+static const u8 sEasyChatWord_Get[] = _("CAPTURA");
+static const u8 sEasyChatWord_Pokedex[] = _("POKéDEX");
+static const u8 sEasyChatWord_Ruby[] = _("RUBÍ");
+static const u8 sEasyChatWord_Level[] = _("NIVEL");
+static const u8 sEasyChatWord_Red[] = _("ROJO");
+static const u8 sEasyChatWord_Green[] = _("VERDE");
+static const u8 sEasyChatWord_Bag[] = _("MOCHILA");
+static const u8 sEasyChatWord_Flame[] = _("LLAMA");
+static const u8 sEasyChatWord_Gold[] = _("ORO");
+static const u8 sEasyChatWord_Leaf[] = _("HOJA");
+static const u8 sEasyChatWord_Silver[] = _("PLATA");
+#endif
+
+static const struct EasyChatWordInfo sEasyChatGroup_Trainer[] = {
+    {
+        .text = sEasyChatWord_IChooseYou,
+        .alphabeticalOrder = 15,
+        .enabled = TRUE
+    },
+    {
+        .text = sEasyChatWord_Gotcha,
+        .alphabeticalOrder = 7,
+        .enabled = TRUE
+    },
+    {
+        .text = sEasyChatWord_Trade,
+        .alphabeticalOrder = 9,
+        .enabled = TRUE
+    },
+    {
+        .text = sEasyChatWord_Sapphire,
+        .alphabeticalOrder = 5,
+        .enabled = TRUE
+    },
+    {
+        .text = sEasyChatWord_Evolve,
+        .alphabeticalOrder = 11,
+        .enabled = TRUE
+    },
+    {
+        .text = sEasyChatWord_Encyclopedia,
+        .alphabeticalOrder = 4,
+        .enabled = TRUE
+    },
+    {
+        .text = sEasyChatWord_Nature,
+        .alphabeticalOrder = 10,
+        .enabled = TRUE
+    },
+    {
+        .text = sEasyChatWord_Center,
+        .alphabeticalOrder = 1,
+        .enabled = TRUE
+    },
+    {
+        .text = sEasyChatWord_Egg,
+        .alphabeticalOrder = 24,
+        .enabled = TRUE
+    },
+    {
+        .text = sEasyChatWord_Link,
+        .alphabeticalOrder = 8,
+        .enabled = TRUE
+    },
+    {
+        .text = sEasyChatWord_SpAbility,
+        .alphabeticalOrder = 2,
+        .enabled = TRUE
+    },
+    {
+        .text = sEasyChatWord_Trainer,
+        .alphabeticalOrder = 22,
+        .enabled = TRUE
+    },
+    {
+        .text = sEasyChatWord_Version,
+        .alphabeticalOrder = 21,
+        .enabled = TRUE
+    },
+    {
+        .text = sEasyChatWord_Pokenav,
+        .alphabeticalOrder = 6,
+        .enabled = FALSE
+    },
+    {
+        .text = sEasyChatWord_Pokemon,
+        .alphabeticalOrder = 18,
+        .enabled = TRUE
+    },
+    {
+        .text = sEasyChatWord_Get,
+        .alphabeticalOrder = 23,
+        .enabled = TRUE
+    },
+    {
+        .text = sEasyChatWord_Pokedex,
+        .alphabeticalOrder = 25,
+        .enabled = TRUE
+    },
+    {
+        .text = sEasyChatWord_Ruby,
+        .alphabeticalOrder = 16,
+        .enabled = TRUE
+    },
+    {
+        .text = sEasyChatWord_Level,
+        .alphabeticalOrder = 14,
+        .enabled = TRUE
+    },
+    {
+        .text = sEasyChatWord_Red,
+        .alphabeticalOrder = 13,
+        .enabled = TRUE
+    },
+    {
+        .text = sEasyChatWord_Green,
+        .alphabeticalOrder = 19,
+        .enabled = TRUE
+    },
+    {
+        .text = sEasyChatWord_Bag,
+        .alphabeticalOrder = 17,
+        .enabled = TRUE
+    },
+    {
+        .text = sEasyChatWord_Flame,
+        .alphabeticalOrder = 0,
+        .enabled = TRUE
+    },
+    {
+        .text = sEasyChatWord_Gold,
+        .alphabeticalOrder = 20,
+        .enabled = TRUE
+    },
+    {
+        .text = sEasyChatWord_Leaf,
+        .alphabeticalOrder = 12,
+        .enabled = TRUE
+    },
+    {
+        .text = sEasyChatWord_Silver,
+        .alphabeticalOrder = 3,
         .enabled = TRUE
     }
 };
