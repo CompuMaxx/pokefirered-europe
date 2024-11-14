b06486874220ec901ef7fd54754d57fef07b2bd1
Acimut <79547805+Acimut@users.noreply.github.com>
2024-11-13 23:01:35 -0500

    Update multiboot_berry_glitch_fix.s

--- data/multiboot_berry_glitch_fix.s
+++ data/multiboot_berry_glitch_fix.s
@@ -1,5 +1,11 @@
 	.section .rodata
 
+
 gMultiBootProgram_BerryGlitchFix_Start::
+	.ifdef ENGLISH
 	.incbin "data/mb_berry_fix.gba"
+	.endif
+	.ifdef SPANISH
+	.incbin "data/mb_berry_fix_es.gba"
+	.endif
 gMultiBootProgram_BerryGlitchFix_End::
